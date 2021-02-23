35899bc9e40801e6692ce66a70bdea1b9344fa4e
Pedro Henrique <pedro.henrique.torres@hotmail.com>
2021-02-23 15:52:56 -0300

    Adicionando favicon

--- moveit/src/pages/index.tsx
+++ moveit/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { CompletedChallenges } from "../components/CompletedChalleges";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
@@ -8,6 +9,10 @@ import styles from "../styles/pages/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
