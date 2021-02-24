577bd5730e6ec21e32f069eeb2e7308ef9555325
Pedro Henrique <pedro.henrique.torres@hotmail.com>
2021-02-24 15:24:40 -0300

    Adicionado e Configurado testes

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,15 +1,6 @@
-import "reflect-metadata";
-import express from "express";
-
-import { router } from "./routes";
-import "./database";
+import { app } from "./app";
 
 const PORT = process.env.PORT || 3333;
-
-const app = express();
-
-app.use(express.json());
-app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server is running at port:${PORT}`);
